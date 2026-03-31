c64b86b51fb9da5b892db36f1d6d76678eeb718e
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-03-31 09:32:37 +0530

    user login functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { userRegisterController } from "../controllers/auth.controller.js";
+import { loginController, userRegisterController } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
+/* POST /api/auth/register */
 authRouter.route("/register").post(userRegisterController) 
+
+/* POST /api/auth/log-in */
+authRouter.route("/log-in").post(loginController)
 
 export { authRouter }
