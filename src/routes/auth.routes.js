784f2016938c3d4160511921305de96fda4e6c91
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-04-17 22:25:16 +0530

    added rate limit to auth and transaction routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { loginController, userRegisterController, refreshAccessToken, logOut } from "../controllers/auth.controller.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js"
+import { authLimiter } from "../middlewares/rateLimiter.middleware.js";
 const authRouter = Router();
 
 /* POST /api/auth/register */
-authRouter.route("/register").post(userRegisterController) 
+authRouter.route("/register").post(authLimiter, userRegisterController) 
 
 /* POST /api/auth/log-in */
-authRouter.route("/log-in").post(loginController)
+authRouter.route("/log-in").post(authLimiter, loginController)
 
 /* 
 - POST /api/auth/refresh-token 
